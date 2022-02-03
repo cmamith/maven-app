490eb5d24945e8ea768ea282c3d94a84379d64a9
amcm <amith.c.m@oracle.com>
2022-02-04 03:15:17 +0530

    changed the value to point to jenkins server

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /Users/amcm/Desktop/DEVOPS-STUDY/AWs/4my-organization.pem /tmp/.auth ec2-user@3.89.88.102:/tmp/.auth
-scp -i /Users/amcm/Desktop/DEVOPS-STUDY/AWs/4my-organization.pem ./jenkins/deploy/publish.sh ec2-user@3.89.88.102:/tmp/publish
-scp -i /Users/amcm/Desktop/DEVOPS-STUDY/AWs/4my-organization.pem ./jenkins/deploy/publish.sh ec2-user@3.89.88.102 "/tmp/publish"
+scp -i /opt/4my-organization.pem /tmp/.auth ec2-user@3.89.88.102:/tmp/.auth
+scp -i /opt/4my-organization.pem ./jenkins/deploy/publish.sh ec2-user@3.89.88.102:/tmp/publish
+scp -i /opt/4my-organization.pem ./jenkins/deploy/publish.sh ec2-user@3.89.88.102 "/tmp/publish"
