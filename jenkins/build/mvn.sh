eb6d818c07f284e61edc51a31bed246de71c3f43
amcm <amith.c.m@oracle.com>
2022-02-04 02:57:47 +0530

    Minor changes

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,6 @@ echo "###########"
 echo "build the jar"
 echo "###########"
 
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v $PWD/java-app:/app -v /tmp/.m2/:/root/.m2/ -w /app maven:3.6-alpine "$@"
+docker run --rm  -v $WORKSPACE/java-app:/app -v /tmp/.m2/:/root/.m2/ -w /app maven:3.6-alpine "$@"
